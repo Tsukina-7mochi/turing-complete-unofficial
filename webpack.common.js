e719f8f79287c5ca61bb4615d7cde105b49b5626
Tsukina-7mochi <tsukina7mochi@gmail.com>
2022-03-28 16:07:57 +0900

    initial commit

---
+++ webpack.common.js
@@ -0,0 +1,69 @@
+/* eslint-env node */
+const path = require('path');
+
+const srcPath  = path.join(__dirname, 'app');
+const destPath = path.join(__dirname, 'docs/app');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: true,
+              sourceMap: true,
+            },
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: true,
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+              sassOptions: {
+                // fiber: require('fibers'),
+                fiber: false
+              },
+              sourceMap: true,
+            },
+          }
+        ],
+      },
+      {
+        test: /.ts$/,
+        use: 'ts-loader'
+      }
+    ]
+  },
+  entry: {
+    main: path.join(srcPath, 'main.ts'),
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: destPath,
+  },
+  resolve: {
+    extensions: ['.js', '.ts']
+  },
+  stats: {
+    builtAt: true,
+    errorsCount: true,
+    warningsCount: true,
+    timings: true,
+  }
+}
