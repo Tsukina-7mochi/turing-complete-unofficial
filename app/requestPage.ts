4accb5140ee37bd4702c36c264252b3ba5dc8bf0
Tsukina-7mochi <tsukina7mochi@gmail.com>
2022-04-01 19:53:26 +0900

    コメントを削除

--- app/requestPage.ts
+++ app/requestPage.ts
@@ -75,56 +75,6 @@ const processMarkdown = function(content_: string, pageName: string): string {
   return content;
 }
 
-// const processHTML = function(content: string): string {
-//   const doc = new DOMParser().parseFromString(content, 'text/html');
-//
-//   // code:truth_tableの書き換え
-//   doc.querySelectorAll('pre > code.language-truth_table').forEach((elCode) => {
-//     const lines = (elCode.textContent ?? '').trim().split('\n').map((line) => line.trim());
-//     const cells = lines.map((line) => line.split(',').map((cell) => cell.trim()));
-//     const tableHeight = cells.map((line) => line.length).reduce((prev, cur) => (prev < cur ? cur : prev), 0);
-//
-//     const table = document.createElement('table');
-//     table.className = 'truth';
-//     const tbody = document.createElement('tbody');
-//     const trs = new Array(tableHeight).fill(0).map(() => document.createElement('tr'));
-//     cells.forEach((line) => {
-//       for(let i = 0; i < tableHeight; i++) {
-//         const td = document.createElement('td');
-//         td.textContent = line[i];
-//         if(line[i] === 'T' || line[i] === '1') {
-//           td.classList.add('T');
-//         }
-//         if(line[i] === 'F' || line[i] === '0') {
-//           td.classList.add('F');
-//         }
-//
-//         trs[i].appendChild(td);
-//       }
-//     });
-//
-//     trs.forEach((tr) => {
-//       tbody.appendChild(tr);
-//     });
-//     table.appendChild(tbody);
-//
-//     // pre > code の直前に挿入
-//     elCode.parentNode?.parentNode?.insertBefore(table, elCode.parentNode);
-//     elCode.parentNode?.parentNode?.removeChild(elCode.parentNode);
-//   });
-//
-//   // スポイラー
-//   doc.querySelectorAll('.spoiler').forEach((spoiler) => {
-//     const controller = document.createElement('div');
-//     controller.className = 'spoiler-controller material-icons';
-//     controller.innerHTML = '&#xE5CF;開く';
-//
-//     spoiler.parentNode?.insertBefore(controller, spoiler);
-//   });
-//
-//   return doc.body.innerHTML;
-// }
-
 const buildTruthTable = function(content: string): string {
   const cells = content.trim().split('\n')
     .map(line => {
